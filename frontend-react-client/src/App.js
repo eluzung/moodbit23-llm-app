d55cc44d21207edd3ff6a2c8d9cd4ba9fd502dfe
eluzung <luzungelaine@gmail.com>
2024-06-18 12:22:24 -0400

    fix: chatbot response gets automatically rendered

--- frontend-react-client/src/App.js
+++ frontend-react-client/src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
-import UserInput from './components/UserInput';
-import Wikipedia from './components/Wikipedia';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import UserInput from "./components/UserInput";
+import Wikipedia from "./components/Wikipedia";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to chatting with ChatBot!</h1>
-      <UserInput/>
-      <h1>Search through Wikipedia!</h1>
-      <Wikipedia/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<UserInput />} />
+          <Route path="/wikipedia" element={<Wikipedia />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+{
+  /* <h1>Welcome to chatting with ChatBot!</h1>
+<UserInput/>
+<h1>Search through Wikipedia!</h1>
+<Wikipedia/> */
+}
--- frontend-react-client/src/components/UserInput.jsx
+++ frontend-react-client/src/components/UserInput.jsx
@@ -1,51 +1,102 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function UserInput() {
-    const [input, setInput] = useState("");
-    const [res, setRes] = useState({});
+  const navigate = useNavigate();
+  const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [messageList, setMessageList] = useState([]);
+  const [state, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
 
-    const fetchResponse = async () => {
-        try {
-            let inputObj = {
-                strInput: input
-            };
-            console.log(inputObj);
-            
-            const response = await axios.post(`https://a92d-108-46-33-124.ngrok-free.app/api/response`, inputObj);
-    
-            console.log("This is the response data:", response.data);
-            } catch (error) {
-                console.error(error);
-            }
-    };
+  const handleWikiPage = () => {
+    navigate("/wikipedia");
+  };
 
-    function handleChange(event) {
-        event.preventDefault();
-        setInput(event.target.value);
-    };
+  const fetchResponse = async () => {
+    setLoading(true);
 
-    async function handleSubmit(event) {
-        event.preventDefault()
-        fetchResponse();
-    };
+    if (!input) return;
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-            <input 
-                type="text"
-                value={input}
-                onChange={handleChange}
-                placeholder="Enter a question"
-            />
-            <button type="submit" id="submit">
-                Enter
-            </button>
-            </form>
-            
-        </div>
-    );
+    let msgArr = messageList;
+    msgArr.push({ role: "User", content: input });
+    setMessageList(msgArr);
+
+    try {
+      let inputObj = {
+        strInput: input,
+      };
+
+      const response = await axios.post(
+        `https://7249-108-46-33-124.ngrok-free.app/api/response`,
+        inputObj
+      );
+
+      msgArr.push({ role: "Chatbot", content: response.data });
+      setMessageList(msgArr);
+      setLoading(false);
+      setInput("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  function handleChange(event) {
+    setInput(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetchResponse();
+    console.log(messageList);
+  }
+
+  function handleMessageList() {
+    if (loading === false) {
+      forceUpdate();
+    }
+  }
+
+  return (
+    <div>
+      <h1>Welcome to chatting with ChatBot!</h1>
+      <button id="wiki-button" onClick={handleWikiPage}>
+        Search the Wiki through chat!
+      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={input}
+          onChange={handleChange}
+          placeholder="Enter a question"
+        />
+        <button type="submit" id="submit">
+          Enter
+        </button>
+      </form>
+      <div onChange={handleMessageList}>
+        {loading ? (
+          <p></p>
+        ) : (
+          <div>
+            {messageList.map((message, index) => (
+              <p
+                key={index}
+                className={message.role === "User" ? message.content : ""}
+              >
+                <span>
+                  <b>{message.role}</b>
+                </span>
+                <span>: </span>
+                <span>{message.content}</span>
+              </p>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default UserInput;
